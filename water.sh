38a9185a80278b6ad52f282825156089f6a5834c
Yue Gao <hologerry@gmail.com>
2023-06-15 16:02:37 +0800

    update

--- water.sh
+++ water.sh
@@ -1,6 +1,76 @@
 python scripts/text_editing_stable_diffusion.py \
-    --prompt "text label of bottle" \
+    --prompt "a water bottle label" \
     --init_image "../bot_data_processing/img_msk_pair/0138_img.jpg" \
     --mask "../bot_data_processing/img_msk_pair/0138_msk.png" \
-    --output_path "outputs/0138_res.png" \
+    --output_path "outputs_2/0138_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0143_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0143_msk.png" \
+    --output_path "outputs_2/0143_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0144_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0144_msk.png" \
+    --output_path "outputs_2/0144_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0148_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0148_msk.png" \
+    --output_path "outputs_2/0148_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0149_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0149_msk.png" \
+    --output_path "outputs_2/0149_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0150_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0150_msk.png" \
+    --output_path "outputs_2/0150_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0153_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0153_msk.png" \
+    --output_path "outputs_2/0153_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0154_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0154_msk.png" \
+    --output_path "outputs_2/0154_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0157_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0157_msk.png" \
+    --output_path "outputs_2/0157_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0160_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0160_msk.png" \
+    --output_path "outputs_2/0160_res.png" \
+
+
+python scripts/text_editing_stable_diffusion.py \
+    --prompt "a water bottle label" \
+    --init_image "../bot_data_processing/img_msk_pair/0161_img.jpg" \
+    --mask "../bot_data_processing/img_msk_pair/0161_msk.png" \
+    --output_path "outputs_2/0161_res.png" \
 
