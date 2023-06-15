fdd64daeed5f8fd48c7d2a699e854c358c0f9b66
Yue Gao <hologerry@gmail.com>
2023-06-15 16:36:47 +0800

    update

--- ncos_real_test.sh
+++ ncos_real_test.sh
@@ -1,5 +1,5 @@
 python scripts/text_editing_stable_diffusion.py \
     --prompt "a tablet opened on the table." \
-    --init_image "../data/NOCS/real_test/scene_2/0000_color.png" \
-    --mask "../data/NOCS/real_test/scene_2/0000_mask_1.png" \
+    --init_image "../data/NOCS/real_test/scene_2/0000_color_pad.png" \
+    --mask "../data/NOCS/real_test/scene_2/0000_mask_pad_1.png" \
     --output_path "outputs_ncos_real_test_scene_2/0000_1.png" \
